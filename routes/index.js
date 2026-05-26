6dd9c010a82159c1528c6d64bf03ca24762f6029
Narek Minasyan <narekminasyan52@gmail.com>
2026-05-27 00:07:57 +0400

    add index.js and app.js codes

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import validation from "../middlewares/validation.js";
+import schema from "../middlewares/schemas/auth.schema.js";
+import authController from "../controllers/auth.js";
+
+const router = Router();
+
+// register
+router.post(
+    '/auth/register',
+    validation(schema.register, 'body'),
+    authController.register
+);
+
+// login
+router.post(
+    '/auth/login',
+    validation(schema.login, 'body'),
+    authController.login
+);
+
+// logout
+router.post('/auth/logout', authController.logout);
+
+// me
+router.get('/auth/me', authController.me);
+
+export default router;
